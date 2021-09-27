df23fbc48b7f2f30b7e6d83a5edd298ecc574b56
Chase X. Codex <chase.x.codex@gmail.com>
2021-09-27 21:45:31 +0300

    Added helpers, circle node, and direction

--- src/circle_node.h
+++ src/circle_node.h
@@ -1,4 +1,22 @@
 #ifndef PATH_FINDER_CPP_CIRCLE_NODE_H
 #define PATH_FINDER_CPP_CIRCLE_NODE_H
 
+#include <vector>
+#include "node.h"
+#include "circle.h"
+
+using namespace std;
+
+typedef Connection pair<Node*, Node*>
+
+struct CircleNode : public Circle {
+    bool generated = false;
+    vector<Connection> connections{};
+    void AddConnection(Connection connection) {
+        connections.push_back(connection);
+    }
+    CircleNode(Circle circle) : Circle(circle) {}
+};
+
+
 #endif //PATH_FINDER_CPP_CIRCLE_NODE_H
---
+++ src/direction.h
@@ -0,0 +1,10 @@
+#ifndef PATH_FINDER_CPP_DIRECTION_H
+#define PATH_FINDER_CPP_DIRECTION_H
+
+enum Direction {
+    Clockwise = 1,
+    CounterClockwise = -1,
+    None = 0
+};
+
+#endif //PATH_FINDER_CPP_DIRECTION_H
